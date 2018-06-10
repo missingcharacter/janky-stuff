509c19be6e9ee0b90687826827b53e4482d01a55
Ricardo Rosales <missingcharacter@gmail.com>
2018-06-10 10:27:28 -0700

    pylint3 10 out of 10 for find_hostnames.py and works in python2 also :)

--- python/find-hostnames/find_hostnames.py
+++ python/find-hostnames/find_hostnames.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Should work with python 2.7.15 and 3.6.5
+"""Reverse DNS lookup for a list of IPs separated by a new line
+Should work with python 2.7.15 and 3.6.5"""
 
 import argparse
 import json
@@ -7,34 +8,43 @@ import os
 import socket
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", required=True,
-                    help="Input file with a list of IPs separated by a new line")
-parser.add_argument("-o", "--output",
-                    help="Output file with a list of IP:hostname pairs separated by a new line")
-args = parser.parse_args()
+PARSER = argparse.ArgumentParser(
+    description="Reverse DNS lookup for a list of IPs separated by a new line"
+)
+PARSER.add_argument(
+    "-i",
+    "--input",
+    required=True,
+    help="Input file with a list of IPs separated by a new line",
+)
+PARSER.add_argument(
+    "-o",
+    "--output",
+    help="Output file with a list of IP:hostname pairs separated by a new line",
+)
+ARGS = PARSER.parse_args()
 
-if not os.path.isfile(args.input):
-    print("{} does not exist".format(args.input))
+if not os.path.isfile(ARGS.input):
+    print("{} does not exist".format(ARGS.input))
     sys.exit(1)
 
-f = open(args.input, "r")
-contents = f.readlines()
+F = open(ARGS.input, "r")
+CONTENTS = F.readlines()
 
-ips_hostnames = {}
+IPS_HOSTNAMES = {}
 
-for ip in contents:
+for ip in CONTENTS:
     ip = ip.rstrip()
-    if not ip == '':
+    if not ip == "":
         try:
             hostname, alias, addresslist = socket.gethostbyaddr(ip)
         except (socket.error, socket.herror, socket.gaierror):
-            hostname = ''
+            hostname = ""
 
-        ips_hostnames[ip] = hostname
-        if args.output == None:
+        IPS_HOSTNAMES[ip] = hostname
+        if ARGS.output is None:
             print("{}:{}".format(ip, hostname))
 
-if args.output:
-    with open(args.output, 'w') as file:
-        file.write(json.dumps(ips_hostnames, indent=2))
+if ARGS.output:
+    with open(ARGS.output, "w") as a_file:
+        a_file.write(json.dumps(IPS_HOSTNAMES, indent=2))
